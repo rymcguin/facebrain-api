50b25e62f8de77712ec0d711499bbf3d24d55470
Ryan McGuiness <rcm421@lehigh.edu>
2020-08-27 18:35:57 -0400

    fixed port and package error

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ app.post("/imageurl", (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(42924, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`app is running on port ${process.env.PORT}`);
 });
