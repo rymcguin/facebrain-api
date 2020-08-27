d0327bd2269a8c7b9aa97f064a3582a8be6dc684
Ryan McGuiness <rcm421@lehigh.edu>
2020-08-27 18:28:31 -0400

    fixed port error

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ app.post("/imageurl", (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(42924, () => {
   console.log(`app is running on port ${process.env.PORT}`);
 });
