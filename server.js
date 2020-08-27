19572cf6c0defe3a268277d263bbf29e1df7c77b
Ryan McGuiness <rcm421@lehigh.edu>
2020-08-27 19:50:28 -0400

    connected to heroku postgres

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require("./controllers/image.js");
 const database = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-pointy-18955",
     user: "mcguiness",
     password: "",
     database: "faceBrain",
